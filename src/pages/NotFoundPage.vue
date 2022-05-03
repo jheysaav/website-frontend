00bc509310f47eb2d8e449b20ab3196c571e7c7c
Jheyson Saavedra <hello@jheysonsaavedra.com>
2022-05-03 18:04:15 -0500

    feat: add basic page not found (404 error)

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,5 @@
+<template>
+  <default-layout title="Not found">
+    <h1>Not found</h1>
+  </default-layout>
+</template>
--- src/plugins/router.ts
+++ src/plugins/router.ts
@@ -1,6 +1,10 @@
 import { createRouter, createWebHistory, RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('@/pages/NotFoundPage.vue'),
+  },
   { path: '/', component: () => import('@/pages/HomePage.vue') },
 ]
 
