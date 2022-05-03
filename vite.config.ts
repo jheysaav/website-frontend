13592e71de6b42f661736882050f1b582476c6e0
Jheyson Saavedra <hello@jheysonsaavedra.com>
2022-05-02 20:19:57 -0500

    perf: remove support for legacy browsers

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import legacy from '@vitejs/plugin-legacy'
 import * as path from 'path'
 
 export default defineConfig({
-  plugins: [vue(), legacy()],
+  plugins: [vue()],
   resolve: {
     alias: [
       { find: '@', replacement: path.join(__dirname, './src') },
